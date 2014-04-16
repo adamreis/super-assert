3b0913559bef8810ba312ff49f5c2e998ed03930
Adam Reis <adamhreis@gmail.com>
2014-04-16 17:30:50 -0400

    replaces empty lines with comments, so they'll be preserved

--- index.js
+++ index.js
@@ -8,10 +8,23 @@ if (process.argv.length != 4) {
   process.exit(1);
 }
 
+function replaceEmptyLines(data) {
+  var lines = data.toString().split('\n');
+  for(var i=0; i<lines.length; i++) {
+    if(lines[i]=='') {
+      lines[i]='//';
+    }
+  }
+  lines.push('//');
+  return lines.join('\n');
+}
+
+
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
+  data = replaceEmptyLines(data);
   var ast = esprima.parse(data, { loc: true,
                                   tokens: true,
                                   range: true,
