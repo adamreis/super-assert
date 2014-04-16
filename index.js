21176d6ce8d9a2e63065d28649e62522ed628404
Adam Reis <adamhreis@gmail.com>
2014-04-16 18:38:58 -0400

    works beautifully for all single-line statements, and multiline function calls like asserts.  Still need to deal with multiline strings, single line if statements, etc

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+//TODO: figure out exactly how escodegen deals with newlines, because it definitely separates one line if statements)
 var fs = require('fs'),
   esprima = require('esprima'),
   escodegen = require('escodegen');
-
 
 if (process.argv.length != 4) {
   console.log('\tUSAGE:\tnode index.js [inputfile.js] [outputFileName.js]');
@@ -18,7 +18,6 @@ function replaceEmptyLines(data) {
   lines.push('//');
   return lines.join('\n');
 }
-
 
 function traverse(node, func) {
   func(node)
@@ -39,6 +38,41 @@ function traverse(node, func) {
   }
 }
 
+function addSecondAssertArgument (ast) {
+  traverse(ast, function(node) {
+    if (node.type == 'ExpressionStatement' &&
+        node.expression.callee &&
+        node.expression.callee.name == 'assert'){
+      line = node.expression.callee.loc.start.line;
+      assertionText = 'line ' + line + ': ' + escodegen.generate(node);
+      node.expression.arguments[1] = {"type":"Literal", "value":assertionText};
+    }
+  });
+}
+
+function fixMultiLineStatements (ast, data) {
+  var missedLines = [];
+  traverse(ast, function(node) {
+    if (node.type == 'ExpressionStatement' ) {
+      var start = node.loc.start.line;
+      var end = node.loc.end.line;
+      if (start != end) {
+        // console.log('---------NODE-------\n'+JSON.stringify(node, null, 2));
+        for (var i = start + 1; i <= end; i++) {
+          missedLines.push(i);
+        }
+      }
+    }
+  });
+
+  console.log('=======missed lines==========\n'+missedLines);
+  var lines = data.toString().split('\n');
+  for (var i = 0 ; i < missedLines.length; i++) {
+    lines.splice(missedLines[i] + i - 1, 0, "//");
+  }
+  return lines.join('\n');
+}
+
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
@@ -49,16 +83,9 @@ fs.readFile(process.argv[2], 'utf8', function (err, data) {
                                   range: true,
                                   comment: true});
   ast = escodegen.attachComments(ast, ast.comments, ast.tokens);
-  traverse(ast, function(node){
-    if (node.type == 'ExpressionStatement' &&
-        node.expression.callee &&
-        node.expression.callee.name == 'assert'){
-      line = node.expression.callee.loc.start.line;
-      assertionText = 'line ' + line + ': ' + escodegen.generate(node);
-      node.expression.arguments[1] = {"type":"Literal", "value":assertionText};
-    }
-  });
-  var out_data = escodegen.generate(ast, {comment: true});
+  addSecondAssertArgument(ast);
+  var regeneratedCode = escodegen.generate(ast, {comment: true});
+  var out_data = fixMultiLineStatements(ast, regeneratedCode);
   fs.writeFile(process.argv[3], out_data, function (err) {
     if (err) {
       console.log(err);
