18faa61aa102d752001a51623f0b33e3d9f19e59
Adam Reis <adamhreis@gmail.com>
2014-04-16 17:45:52 -0400

    adds second argument to Assertions

--- index.js
+++ index.js
@@ -20,6 +20,25 @@ function replaceEmptyLines(data) {
 }
 
 
+function traverse(node, func) {
+  func(node)
+  for (var key in node) {
+    if (node.hasOwnProperty(key)) {
+      var child = node[key];
+      if (typeof child === 'object' && child !== null) {
+
+        if (Array.isArray(child)) {
+          child.forEach(function(node) {
+            traverse(node, func);
+          });
+        } else {
+          traverse(child, func);
+        }
+      }
+    }
+  }
+}
+
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
@@ -30,6 +49,15 @@ fs.readFile(process.argv[2], 'utf8', function (err, data) {
                                   range: true,
                                   comment: true});
   ast = escodegen.attachComments(ast, ast.comments, ast.tokens);
+  traverse(ast, function(node){
+    if (node.type == 'ExpressionStatement' &&
+        node.expression.callee &&
+        node.expression.callee.name == 'assert'){
+      line = node.expression.callee.loc.start.line;
+      assertionText = 'line ' + line + ': ' + escodegen.generate(node);
+      node.expression.arguments[1] = {"type":"Literal", "value":assertionText};
+    }
+  });
   var out_data = escodegen.generate(ast, {comment: true});
   fs.writeFile(process.argv[3], out_data, function (err) {
     if (err) {
